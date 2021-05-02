27a8a83b040cc8aaad17ce88ec16921c510fa91b
Cyanide3D <gamer1997235@yandex.ru>
2021-05-02 23:27:01 +0300

    232

--- src/main/java/com/example/Pr16/models/Card.java
+++ src/main/java/com/example/Pr16/models/Card.java
@@ -11,7 +11,7 @@ public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Basic
+    @Column(name = "card_number")
     private int cardNumber;
     @Basic
     private int code;
